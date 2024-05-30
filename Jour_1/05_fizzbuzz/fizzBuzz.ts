f4e35ddbba13ee8707bde387733792cf6c0277e5
zoeleca <zoennts@gmail.com>
2024-05-30 11:05:40 +0200

    updated fizzbuzz function to better answer the initial rules

--- Jour_1/05_fizzbuzz/fizzBuzz.ts
+++ Jour_1/05_fizzbuzz/fizzBuzz.ts
@@ -7,3 +7,18 @@ export function fizzBuzz(number: number) {
     return "Buzz";
   } else return number;
 }
+
+export function fizzBuzzfrom1to100() {
+  let array: any[] = [];
+  for (let number = 1; number < 101; number++) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      array.push("FizzBuzz");
+    } else if (number % 3 === 0) {
+      array.push("Fizz");
+    } else if (number % 5 === 0) {
+      array.push("Buzz");
+    } else array.push(number);
+  }
+  return array;
+}
+console.log(fizzBuzzfrom1to100());
